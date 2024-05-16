8466e14eae9814dfb31abc8cb217a4463a6a240b
PokiikoP <152090657+PokiikoP@users.noreply.github.com>
2024-05-16 14:07:57 +0530

    apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "343a1412cb5db78345ac159ae072fe69",
   base: "https://api.openweathermap.org/data/2.5/",
 };
